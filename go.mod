c0e83e1c199e3719d13f5545e6168b43cf44b67d
asafz <asafz@jfrog.com>
2021-12-26 13:45:35 +0200

    stam

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jfrog-qa/helloGoV2CompatibleRootProject/v2
+module github.com/jfrog-qa/helloGoV2CompatibleRootProject/v3
 
 go 1.16
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package hello
 
 func Hello() string {
-    return "Hello from helloGoV2CompatibleRootProject/v2"
+    return "Hello from helloGoV2CompatibleRootProject/v3"
 }
